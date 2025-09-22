87edb6300baddeef639734788b2f15b6d5885c57
Branko <122445060+Stokuca@users.noreply.github.com>
2025-09-22 13:06:43 +0200

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,10 @@ import "./index.css";
 import App from "./App";
 import SessionProvider from "./providers/SessionProvider";
 
-
+/*In a real production environment, some features could be implemented in a more optimal way. 
+For example, socket integration with the frontend could be improved so that when a single piece of data changes, 
+only that specific data is refreshed in the UI. Currently, 
+the frontend refetches all data on any update, which is not the most efficient approach */
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
